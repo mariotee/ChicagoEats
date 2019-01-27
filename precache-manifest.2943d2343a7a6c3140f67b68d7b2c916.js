c542ba1f2739a0611be48e8e846cb9c9ede2c424
Mario Torres <mariotee10@gmail.com>
2019-01-27 01:47:22 -0600

    Updates

---
+++ precache-manifest.2943d2343a7a6c3140f67b68d7b2c916.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "40ef7ce27e5819076f02687b87390f4a",
+    "url": "/ChicagoEats/static/media/logo.40ef7ce2.svg"
+  },
+  {
+    "revision": "faca2a00fe3bb24db611",
+    "url": "/ChicagoEats/static/js/runtime~main.faca2a00.js"
+  },
+  {
+    "revision": "2766072e3b26f3411a73",
+    "url": "/ChicagoEats/static/js/main.2766072e.chunk.js"
+  },
+  {
+    "revision": "7bbf54260729167c624b",
+    "url": "/ChicagoEats/static/js/1.7bbf5426.chunk.js"
+  },
+  {
+    "revision": "2766072e3b26f3411a73",
+    "url": "/ChicagoEats/static/css/main.889dddfa.chunk.css"
+  },
+  {
+    "revision": "31243d4e7328fa48fc0a1301d9b272aa",
+    "url": "/ChicagoEats/index.html"
+  }
+];
